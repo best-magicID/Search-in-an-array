870484785bb738afd06506160807fff3a9a85712
Andrey <58469691+best-magicID@users.noreply.github.com>
2020-08-11 20:16:44 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Поиск_по_массиву
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.информацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.инфоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияОРазработчикеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Задать массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(474, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Последовательный поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(383, 36);
+            this.textBox2.MaxLength = 10;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(296, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Позиция числа:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(429, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 52);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Бинарный поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(185, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите искомое число";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(296, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Искомое число:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(429, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.информацияToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(505, 25);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // информацияToolStripMenuItem
+            // 
+            this.информацияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.инфоToolStripMenuItem,
+            this.информацияОРазработчикеToolStripMenuItem});
+            this.информацияToolStripMenuItem.Name = "информацияToolStripMenuItem";
+            this.информацияToolStripMenuItem.Size = new System.Drawing.Size(99, 21);
+            this.информацияToolStripMenuItem.Text = "Информация";
+            // 
+            // инфоToolStripMenuItem
+            // 
+            this.инфоToolStripMenuItem.Name = "инфоToolStripMenuItem";
+            this.инфоToolStripMenuItem.Size = new System.Drawing.Size(255, 22);
+            this.инфоToolStripMenuItem.Text = "Инфо";
+            // 
+            // информацияОРазработчикеToolStripMenuItem
+            // 
+            this.информацияОРазработчикеToolStripMenuItem.Name = "информацияОРазработчикеToolStripMenuItem";
+            this.информацияОРазработчикеToolStripMenuItem.Size = new System.Drawing.Size(255, 22);
+            this.информацияОРазработчикеToolStripMenuItem.Text = "Информация о разработчике";
+            this.информацияОРазработчикеToolStripMenuItem.Click += new System.EventHandler(this.информацияОРазработчикеToolStripMenuItem_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(8, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Отсорт. массив:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(136, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(405, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(505, 266);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поиск по массиву";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem информацияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem инфоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияОРазработчикеToolStripMenuItem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Поиск_по_массиву
+{
+    public partial class Form1 : Form
+    {
+        public int pozition = 0, dlina_mass;
+        Random rand = new Random();
+        public int[] massiv;
+        public int poisk;
+
+        public Form1() // запуск проги
+        {
+            InitializeComponent();
+            label2.Text = "";
+            label5.Text = "";
+            label7.Text = "";
+            textBox1.HideSelection = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // заполнить массив
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label2.Text = "";
+            label5.Text = "";
+            label7.Text = "";
+            dlina_mass = 15;
+            massiv = new int[dlina_mass];
+            for (int i = 0; i < massiv.Length; i++) //вывод массива
+            {
+                massiv[i] = rand.Next(-99, 99);
+                textBox1.Text += massiv[i] + "  ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // последовательный поиск
+        {
+            label2.Text = ""; // счетчик
+            label5.Text = ""; // искомое число
+            label7.Text = ""; // отсортированный массив
+            try
+            {
+                if (textBox1.Text.IndexOf(textBox2.Text, pozition) >= 0) //выделение элемента
+                {
+                    if (pozition <= textBox1.Text.Length)
+                    {
+                        textBox1.SelectionStart = textBox1.Text.IndexOf(textBox2.Text, pozition);
+                        textBox1.SelectionLength = textBox2.Text.Length;
+                        pozition = textBox1.SelectionStart + 1;
+                    }
+                }
+                else
+                {
+                    pozition = 0;
+                }
+
+                int i;
+                bool Flag = false;
+                poisk = Convert.ToInt32(textBox2.Text);  
+
+                for (i = 0; i < massiv.Length; i++)      //поиск элемента
+                {
+                    if (massiv[i] == poisk)
+                    {
+                        label2.Text = Convert.ToString(i + 1);
+                        label5.Text = Convert.ToString(massiv[i]);
+                        Flag = true;
+                        break;
+                    }
+                }
+                if (Flag == false)
+                    MessageBox.Show("Данного числа нет в массиве", "", MessageBoxButtons.OK);
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте введенное значение!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void информацияОРазработчикеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработал:\nПолчихин А.А.\n2019 год", "Инфо", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.SelectAll();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = "";
+            //textBox2.Text = "";
+            label2.Text = "";
+            label5.Text = "";
+            //label7.Text = "";
+             int i = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Бинарный поиск
+        {
+            label2.Text = "";
+            label5.Text = "";
+            label7.Text = "";
+            try
+            {
+                int[] massiv_Bin = new int[dlina_mass];
+                Array.Copy(massiv, massiv_Bin, 15);
+               // massiv_Bin = massiv;
+                Array.Sort(massiv_Bin);
+                int poisk2 = Convert.ToInt32(textBox2.Text);
+
+                for (int i1 = 0; i1 < massiv_Bin.Length; i1++) //поиск позиции
+                {
+                    if (massiv_Bin[i1] == poisk2)
+                    {
+                        label2.Text = Convert.ToString(i1+ 1);
+                        break;
+                    }
+                }
+
+                foreach (int elem in massiv_Bin) //показ сортированного массива
+                {
+                    label7.Text += elem + " ";
+                }
+
+                
+                int left, right, seredina;
+                left = 0;
+                right = massiv_Bin.Length;
+
+                bool flag2 = false;
+
+                for (int i = 0; i < massiv_Bin.Length; i++) //бинарный поиск
+                {
+                    if (poisk == massiv_Bin.Max())
+                    {
+                        label5.Text = massiv_Bin.Max().ToString();
+                        flag2 = true;
+                        break;
+                    }
+                    if (poisk == massiv_Bin.Min())
+                    {
+                        label5.Text = massiv_Bin.Min().ToString();
+                        flag2 = true;
+                        break;
+                    }
+                    seredina = (left + right) / 2;
+                    if (massiv_Bin[seredina] == poisk)
+                    {
+                        label5.Text =  massiv_Bin[seredina].ToString();
+                        flag2 = true;
+                        break;
+                    }
+                    else
+                    {
+                        if (poisk > massiv_Bin[seredina])
+                            left = seredina + 1;
+                        else
+                            right = seredina ;
+                    }
+                }
+                if (flag2 == false)
+                    MessageBox.Show("Элемент не найден!", "", MessageBoxButtons.OK);
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте введенное значение!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
